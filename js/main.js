595b3e94d3e103ca932c57a0c6be1000f39694e9
abby-garcia <acg361@gmail.com>
2016-03-10 12:31:40 -0500

    animation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
+
+
+
+
+
+
+
 $(function(){
+
   $('#search-term').submit(function(event){
     event.preventDefault();
     var searchTerm = $('#query').val();
@@ -9,11 +17,11 @@ $(function(){
 
 
 function getRequest(searchTerm){
-  var params = {
-    part: 'snippet',
-    key: 'AIzaSyD1GKcq922Qe8V0Z0UWDT-PI1CpoFIx2ZQ',
-    q:'prank',
-    maxResults:6,
+  var params = {  // why do we need to put quotes around part, key, q, maxResults?
+    "part": 'snippet',
+    "key": 'AIzaSyD1GKcq922Qe8V0Z0UWDT-PI1CpoFIx2ZQ',
+    "q":'prank',
+    "maxResults":6,
     order: 'viewCount'
   };
   url = 'https://www.googleapis.com/youtube/v3/search';
@@ -28,7 +36,7 @@ function showResults(videos){
   var html = "";
   $.each(videos, function(index,video){
     console.log(video.snippet.thumbnails.medium.url);
-      html = html + "<li><p>" + video.snippet.title +
+      html = html + "<li><p class= 'results'>" + video.snippet.title +
         "</p><img src='" +  video.snippet.thumbnails.high.url + "'/></li>" ;
   });
   $('#search-results').html(html);
